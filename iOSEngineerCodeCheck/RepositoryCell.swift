50f3ec41004cb42735ef245bb7a57386a1df8981
r0ar-69 <shun199969kiyo@gmail.com>
2021-02-23 20:51:57 +0900

    add: ファイルの追加
    Repositoryを表示するセルのファイルを追加

---
+++ iOSEngineerCodeCheck/RepositoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepositoryCell.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 清浦駿 on 2021/02/23.
+//  Copyright © 2021 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
