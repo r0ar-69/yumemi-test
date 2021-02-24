5545738dd57971ed78897bfe4340d0f09148b4ff
r0ar-69 <shun199969kiyo@gmail.com>
2021-02-24 23:04:49 +0900

    update: UIの修正
    配置を変え、スペースを埋めるためにアイコンを配置

--- iOSEngineerCodeCheck/Detail/DetailViewController.swift
+++ iOSEngineerCodeCheck/Detail/DetailViewController.swift
@@ -23,6 +23,8 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var issuesIconLbl: UILabel!
     @IBOutlet weak var descriptionLbl: UILabel!
     @IBOutlet weak var decorationLbl: UILabel! // text is "Writtn In"
+    @IBOutlet weak var gitHubLogoLbl: UILabel!
+    @IBOutlet weak var gitHubIconLbl: UILabel!
     
     internal var repository: Repo?
     
@@ -55,6 +57,10 @@ class DetailViewController: UIViewController {
         forksIconLbl.text = String.fontOcticon("repo-forked")
         issuesIconLbl.font = UIFont.icon(from: .octicon, ofSize: 18)
         issuesIconLbl.text = String.fontOcticon("issue-opened")
+        gitHubIconLbl.font = UIFont.icon(from: .fontAwesome5Brand, ofSize: 85)
+        gitHubIconLbl.text = String.fontAwesome5Icon("github")
+        gitHubLogoLbl.font = UIFont.icon(from: .octicon, ofSize: 45)
+        gitHubLogoLbl.text = String.fontOcticon("logo-github")
     }
     
     private func getImage(repo: Repo){
