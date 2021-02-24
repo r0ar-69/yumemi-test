bc3decda082bb5922773477b56f687c89dd1c8b0
r0ar-69 <shun199969kiyo@gmail.com>
2021-02-24 14:55:00 +0900

    update: hexから色を取得できるUIColorのextensionを追加

--- iOSEngineerCodeCheck/RepositoryCell.swift
+++ iOSEngineerCodeCheck/RepositoryCell.swift
@@ -23,19 +23,23 @@ class RepositoryCell: UITableViewCell {
         // Initialization code
     }
     
-    func setCell(repo: Repo) {
+    func setCell(repo: Repo, languageColor: String) {
         iconLbl.font = UIFont.icon(from: .octicon, ofSize: 15)
         iconLbl.text = String.fontOcticon("repo")
         starIconLbl.font = UIFont.icon(from: .octicon, ofSize: 16)
         starIconLbl.text = String.fontOcticon("star")
         languageIconLbl.font = UIFont.icon(from: .fontAwesome5Solid, ofSize: 10)
         languageIconLbl.text = String.fontAwesome5Icon("circle")
+        if repo.language != nil {
+            languageIconLbl.textColor = UIColor.hex(string: languageColor, alpha: 1)
+        } else {
+            languageIconLbl.isHidden = true
+        }
         
         fullNameLbl.text = repo.fullName
         descriptionLbl.text = repo.description
         starCountLbl.text = String(repo.stargazersCount)
         languageLbl.text = repo.language
-        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -44,4 +48,20 @@ class RepositoryCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+}
+
+extension UIColor {
+    class func hex ( string : String, alpha : CGFloat) -> UIColor {
+        let string_ = string.replacingOccurrences(of: "#", with: "")
+        let scanner = Scanner(string: string_ as String)
+        var color: UInt32 = 0
+        if scanner.scanHexInt32(&color) {
+            let r = CGFloat((color & 0xFF0000) >> 16) / 255.0
+            let g = CGFloat((color & 0x00FF00) >> 8) / 255.0
+            let b = CGFloat(color & 0x0000FF) / 255.0
+            return UIColor(red:r,green:g,blue:b,alpha:alpha)
+        } else {
+            return UIColor.white;
+        }
+    }
 }
